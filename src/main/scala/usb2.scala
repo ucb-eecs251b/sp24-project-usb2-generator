ffa202b5a6d06824667a10a394eae2a68cfc41dd
Howard Liang <hao-wei.liang@berkeley.edu>
2024-04-25 20:38:54 -0700

    update IO and questions

--- src/main/scala/usb2.scala
+++ src/main/scala/usb2.scala
@@ -31,84 +31,42 @@ case class Usb2Params(
   width: Int = 8,
 )
 
-class UTMI(params: Usb2Params, beatBytes: Int)
-  extends Module {
-    val io = IO(new Bundle {
-    // UTMI Controls
-    val utmi_xcvr_select = Input(UInt(1.W))  // Transceiver Select
-    val utmi_term_select = Input(UInt(1.W))  // Termination Select
-    val utmi_suspend_n   = Input(UInt(1.W))  // Active-Low Suspend 
-    val utmi_line_state  = Output(UInt(2.W)) // Received Line State
-    val utmi_opmode      = Input(UInt(2.W))  // Operation Mode
-
-    // UTMI TX 
-    val utmi_datain      = Input(UInt(params.width.W)) // TX Data Input
-    val utmi_tx_valid    = Input(Bool())               // TX Valid 
-    val utmi_tx_ready    = Output(Bool())              // TX Ready
-
-    // UTMI RX 
-    val utmi_dataout     = Output(UInt(params.width.W)) // RX Data Output
-    val utmi_rx_valid    = Output(Bool())               // RX Valid
-    val utmi_rx_active   = Output(Bool())               // RX Active
-    val utmi_rx_error    = Output(Bool())               // RX Error
-
-    // %%% Do we need to specify a clock and a reset for our block?
-    val clock = Input(Clock()) // %%% Clock()? The 480MHz clk
-    val reset = Input(Bool())
-    val utmi_clk = Output(UInt(1.W)) // %%% UInt(1.W)? The 30/60MHz clk
-  })
-
-  // %%% RX + TX here (Or put RX, TX in Usb2Top?) Also, where should we put the async FIFO?
-  val Usb2RxTop = Module(new Usb2RxTop(params.width.W))
-  Usb2RxTop.io.utmi_datain // %%% To Rx team: This should be a single bit data. Where can we get this data, from DP/DM? 
-  // %%% If we need DP/DM, should we just write this UTMI module inside Usb2Top?
-  Usb2RxTop.io.utmi_clk   := io.utmi_clk
-  Usb2RxTop.io.utmi_reset := io.reset
-  io.utmi_dataout         := Usb2RxTop.io.utmi_dataout
-  io.utmi_rx_valid        := Usb2RxTop.io.utmi_rx_valid
-  io.utmi_rx_active       := Usb2RxTop.io.utmi_rx_active
-  io.utmi_rx_error        := Usb2RxTop.io.utmi_rx_error
-  // %%% How should we handle these signals?
-  Usb2RxTop.io.cru_fs_vp     := io.cru_fs_vp    
-  Usb2RxTop.io.cru_fs_vm     := io.cru_fs_vm    
-  Usb2RxTop.io.cru_hs_vp     := io.cru_hs_vp    
-  Usb2RxTop.io.cru_hs_vm     := io.cru_hs_vm    
-  Usb2RxTop.io.cru_hs_toggle := io.cru_hs_toggle
-  Usb2RxTop.io.cru_clk       := io.cru_clk      
-
-  val Usb2TxTop = Module(new USBTx(params.width.W))
-  Usb2TxTop.io.in.bits  := io.utmi_datain
-  Usb2TxTop.io.in.valid := io.utmi_tx_valid
-  io.utmi_tx_ready      := Usb2TxTop.io.in.ready
-  // %%% How should we handle these signals?
-  io.rpuEn     := Usb2TxTop.io.rpuEn    
-  io.vpo       := Usb2TxTop.io.vpo      
-  io.oeb       := Usb2TxTop.io.oeb      
-  io.hsData    := Usb2TxTop.io.hsData   
-  io.hsDriveEn := Usb2TxTop.io.hsDriveEn
-  io.hsCsEn    := Usb2TxTop.io.hsCsEn   
-
-}
-
 class Usb2Top(params: Usb2Params, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
 
   val io: Usb2TopIO
-  val clock: Clock
+  val clock: Clock // 480MHz
   val reset: Reset
 
-  // Allow restart of UTMI at any point.
-  ex.ready := true.B
-  // Hold reset for at least one full cycle after EX register written.
-  val pastValid = RegInit(false.B)
-  pastValid := ex.valid
-  utmiReset := false.B
-  when (ex.valid || pastValid) {
-    utmiReset := true.B
-  }
+  // Question: Where should we put the async FIFO?
 
-  val utmi = withReset(utmiReset) {
-    Module(new UTMI(params, beatBytes))
-  }
+  // RX
+  val usb2RxLogic = Module(new Usb2RxTop(params.width.W))
+  usb2RxLogic.io.utmi_datain // To Rx team: This should be a single bit data. Question: where can we get this data, from DP/DM? 
+  usb2RxLogic.io.utmi_clk   := pll.io.utmi_clk // Question: where can we get this clock? From pll?
+  usb2RxLogic.io.utmi_reset := reset
+  // Question: How should we handle these signals? From cru?
+  // val cru = Module(new ??)
+  usb2RxLogic.io.cru_fs_vp     := cru.io.cru_fs_vp    
+  usb2RxLogic.io.cru_fs_vm     := cru.io.cru_fs_vm    
+  usb2RxLogic.io.cru_hs_vp     := cru.io.cru_hs_vp    
+  usb2RxLogic.io.cru_hs_vm     := cru.io.cru_hs_vm    
+  usb2RxLogic.io.cru_hs_toggle := cru.io.cru_hs_toggle
+  usb2RxLogic.io.cru_clk       := cru.io.cru_clk      
+
+  // TX
+  val usb2TxLogic = Module(new USBTx(params.width.W))
+  val utmi_datain = RegInit(0.U(params.width.W))
+  usb2TxLogic.io.in.bits  := utmi_datain
+  val utmi_tx_valid = RegInit(false.B)
+  usb2TxLogic.io.in.valid := utmi_tx_valid
+  // Question: How should we handle these signals? To TX?
+  // val tx = Module(new ??)
+  tx.io.rpuEn     := usb2TxLogic.io.rpuEn    
+  tx.io.vpo       := usb2TxLogic.io.vpo      
+  tx.io.oeb       := usb2TxLogic.io.oeb      
+  tx.io.hsData    := usb2TxLogic.io.hsData   
+  tx.io.hsDriveEn := usb2TxLogic.io.hsDriveEn
+  tx.io.hsCsEn    := usb2TxLogic.io.hsCsEn   
 
     val mmio_device = new SimpleDevice("LoopBack", Seq("eecs251b,usb2")) 
     val mmio_node = TLRegisterNode(Seq(AddressSet(params.address, 4096-1)), mmio_device, "reg/control", beatBytes=beatBytes)
@@ -122,13 +80,28 @@ class Usb2Top(params: Usb2Params, beatBytes: Int)(implicit p: Parameters) extend
         // Instantiate TileLink communication module
         withClockAndReset(clock, reset) {
 
-           val data_buffer = Module(new Queue(UInt(params.width.W), params.depth))
+           val data_buffer = Module(new Queue(UInt(params.width.W), params.depth)) // Question: What is this?
 
-           mmio_node.regmap(
+           mmio_node.regmap( // Question: MMIO takes multiple cycles
               0x00 -> Seq(
                 RegField.w(params.width, data_buffer.io.enq)),
               0x04 -> Seq(
-                RegField.r(params.width, data_buffer.io.deq)))
+                RegField.r(params.width, data_buffer.io.deq)),
+              0x08 -> Seq(
+                RegField.r(params.width, usb2RxLogic.io.utmi_dataout)),
+              0x0C -> Seq(
+                RegField.r(1, usb2RxLogic.io.utmi_rx_valid)),  
+              0x10 -> Seq(
+                RegField.r(1, usb2RxLogic.io.utmi_rx_active)), 
+              0x10 -> Seq(
+                RegField.r(1, usb2RxLogic.io.utmi_rx_error)),
+              0x14 -> Seq(
+                RegField.w(params.width, utmi_datain)),
+              0x18 -> Seq(
+                RegField.w(1, utmi_tx_valid)),
+              0x18 -> Seq(
+                RegField.r(1, usb2TxLogic.io.in.ready))
+           )
             
         }
 
