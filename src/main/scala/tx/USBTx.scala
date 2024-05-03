f2f2c44afd00c9cf3f502be32ce0fc6346432686
avolkov <avolkov@berkeley.edu>
2024-05-02 17:41:32 -0700

    SyncGen upd

--- src/main/scala/tx/USBTx.scala
+++ src/main/scala/tx/USBTx.scala
@@ -11,16 +11,24 @@ object TxStates {
   }
 }
 
+object Delimiters {
+  val hsEOP = "h7F".U(8.W)
+  val hsSOFEOP = "h7FFFFFFFF".U(40.W)
+}
+
+
 // Modified TxFSM with added serializer controls
 class TxFSM(dWidth: Int) extends Module {
   import TxStates.State
   import TxStates.State._
+  import Delimiters._
 
   val io = IO(new Bundle {
     val tx_valid_i = Input(Bool())           // Valid input from upstream
     val tx_data_i  = Input(UInt(dWidth.W))   // Data input from upstream
     val serial_clk = Input(Clock())          // Clock input
-    val tx_ready_i = Input(Bool())           // Ready input from downstream
+    val serReady = Input(Bool())           
+    val sofDetect = Input(Bool())
     
     val tx_data_o  = Output(UInt(dWidth.W))  // Data output to downstream
     val tx_valid_o = Output(Bool())          // Valid output to downstream
@@ -30,59 +38,61 @@ class TxFSM(dWidth: Int) extends Module {
 
   val state   = RegInit(State.Idle)
   val data    = RegInit(0.U(32.W))
-  val bit_cnt = RegInit((31.U(5.W)))
+  val bit_cnt = RegInit((0.U(6.W)))
 
   // Initialize outputs
   io.tx_data_o := DontCare
   io.tx_valid_o := false.B
-  io.tx_ready_o := false.B  // It should be false initially
+  io.tx_ready_o := false.B
   io.bStuffDis := false.B
 
   withClock(io.serial_clk) {
     switch(state) {
       is(State.Idle) {
-        io.tx_ready_o := true.B  // FSM is ready to receive data
         when(io.tx_valid_i) {
           state := State.SyncGen
           data := "hAAAAAAAB".U
-          io.tx_valid_o := true.B
+          bit_cnt := 32.U
+        }.otherwise {
+          state := State.Idle
+          io.tx_ready_o := true.B
         }
       }
       is(State.SyncGen) {
-        when(io.tx_ready_i && (bit_cnt === 0.U)) {
+        when(io.serReady && (bit_cnt === 0.U)) {
           state := State.DataTx
-        }
-        when(io.tx_ready_i && (bit_cnt > 24.U && bit_cnt <= 32.U)) {
-          io.tx_data_o := data(31, 24)
-          bit_cnt := bit_cnt - 1.U
+        }.elsewhen(io.serReady && (bit_cnt > 0.U && bit_cnt <= 32.U)) {
+          val byteIdx = (bit_cnt - 1.U) >> 3
+          io.tx_data_o := MuxCase(DontCare, Array(
+            (byteIdx === 3.U) -> data(31, 24),
+            (byteIdx === 2.U) -> data(23, 16),
+            (byteIdx === 1.U) -> data(15, 8),
+            (byteIdx === 0.U) -> data(7, 0)
+          ).toIndexedSeq)
+          io.tx_valid_o := true.B
           state := State.SyncGen
-        }.elsewhen(io.tx_ready_i && (bit_cnt > 16.U && bit_cnt <= 24.U)) {
-          io.tx_data_o := data(23, 16)
           bit_cnt := bit_cnt - 1.U
-          state := State.SyncGen
-        }.elsewhen(io.tx_ready_i && (bit_cnt > 8.U && bit_cnt <= 16.U)) {
-          io.tx_data_o := data(15, 8)
-          bit_cnt := bit_cnt - 1.U
-          state := State.SyncGen
-        }.elsewhen(io.tx_ready_i && (bit_cnt > 0.U && bit_cnt <= 8.U)) {
-          io.tx_data_o := data(7, 0)
-          bit_cnt := bit_cnt - 1.U
+        }.otherwise {
           state := State.SyncGen
         }
       }
       is(State.DataTx) {
         io.tx_data_o  := io.tx_data_i
         io.tx_valid_o := true.B
-        io.bStuffDis := true.B
-        when(io.tx_valid_i && io.tx_ready_i) {
+        when(!io.tx_valid_i && io.serReady) {
+          io.bStuffDis := true.B
           state := State.EOPGen
-          io.tx_data_o := "b01111111".U
-          bit_cnt := 8.U
+          when(io.sofDetect) {
+            io.tx_data_o := Delimiters.hsSOFEOP(39, 32)
+            bit_cnt := 40.U
+          }.otherwise {
+            io.tx_data_o := Delimiters.hsEOP
+            bit_cnt := 8.U
+          }
         }
       }
       is(State.EOPGen) {
-
-        when(bit_cnt === 0.U && io.tx_ready_i) {
+        when(bit_cnt === 0.U && io.serReady) {
           state := State.Idle
           io.tx_valid_o := false.B 
         }.elsewhen(bit_cnt > 0.U && bit_cnt < 3.U) {
@@ -114,16 +124,16 @@ class USBTx(dWidth: Int) extends Module {
                                           // 00: Normal, 
                                           // 01: Non-driving (propagate to Tx Driver => remove termination)
                                           // 10: BitStuffing and NRZI disabled
+    val sofDetectSIE = Input(UInt(1.W))   // Start of Frame detection from SIE
+    val fsDout       = Output(UInt(1.W))   
+    val seo          = Output(UInt(1.W))
+    val oe           = Output(UInt(1.W))
 
-    val fsDout      = Output(UInt(1.W))   
-    val seo         = Output(UInt(1.W))
-    val oe          = Output(UInt(1.W))
+    val rpuEn        = Output(UInt(1.W))    // Pull-up resistor enable: Asserted when the device is ready
 
-    val rpuEn       = Output(UInt(1.W))    // Pull-up resistor enable: Asserted when the device is ready
-
-    val hsDout      = Output(UInt(1.W))
-    val hsDriveEn   = Output(UInt(1.W))
-    val hsCurrEn    = Output(Bool())
+    val hsDout       = Output(UInt(1.W))
+    val hsDriveEn    = Output(UInt(1.W))
+    val hsCurrEn     = Output(Bool())
   })
   
 
@@ -136,17 +146,18 @@ class USBTx(dWidth: Int) extends Module {
   fsm.io.tx_valid_i := io.pDin_packet.valid
   fsm.io.tx_data_i  := io.pDin_packet.bits
   fsm.io.serial_clk := io.serialClk
+  fsm.io.sofDetect  := io.sofDetectSIE
 
   // FSM <-> Serializer
   serializer.io.pDataIn.valid := fsm.io.tx_valid_o
   serializer.io.pDataIn.bits  := fsm.io.tx_data_o
   serializer.io.clockOut      := io.serialClk
   serializer.io.xcvrSelect    := io.xcvrSel
-  fsm.io.tx_ready_i           := serializer.io.pDataIn.ready
+  fsm.io.serReady             := serializer.io.pDataIn.ready
 
   withClock(io.serialClk) {
     when(io.opMode === 0.U) {
-      bitStuffer.io.en := !fsm.io.bStuffDis
+      bitStuffer.io.en := !fsm.io.bStuffDis        
       nrziEnc.io.en := true.B
     }.otherwise {
       bitStuffer.io.en := false.B
